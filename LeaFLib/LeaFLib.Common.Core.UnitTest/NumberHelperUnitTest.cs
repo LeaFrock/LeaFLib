a92d9c15d401ad1195d0c9c684e63660214d5539
LeaFrock <petergeorge@126.com>
2022-08-22 18:14:15 +0800

    Rewrite `NumberHelper`

--- LeaFLib/LeaFLib.Common.Core.UnitTest/NumberHelperUnitTest.cs
+++ LeaFLib/LeaFLib.Common.Core.UnitTest/NumberHelperUnitTest.cs
@@ -5,19 +5,26 @@ namespace LeaFLib.Common.Core.UnitTest
     public class NumberHelperUnitTest
     {
         [Theory]
-        [InlineData(31)]
-        [InlineData(32)]
-        [InlineData(1641385434)]
-        public void ConvertInt32ToBin32(int num)
+        [InlineData(31, "v")]
+        [InlineData(32, "10")]
+        [InlineData(1641385434, "1gtb3eq")]
+        [InlineData(int.MaxValue, "1vvvvvv")]
+        public void FormatInt32To32Base(int num, string expected)
         {
-            string expected = num switch
-            {
-                31 => "v",
-                32 => "10",
-                1641385434 => "1gtb3eq",
-                _ => throw new System.NotImplementedException()
-            };
-            string str = NumberHelper.ConvertToBin32(num, "0123456789abcdefghijklmnopqrstuv");
+            const string customAlphabet = "0123456789abcdefghijklmnopqrstuv";
+            string str = NumberHelper.FormatInt32To32Base(num, customAlphabet);
+            Assert.Equal(expected, str);
+        }
+
+        [Theory]
+        [InlineData(31L, "v")]
+        [InlineData(32L, "10")]
+        [InlineData(1641385434L, "1gtb3eq")]
+        [InlineData(long.MaxValue, "7vvvvvvvvvvvv")]
+        public void FormatInt64To32Base(long num, string expected)
+        {
+            const string customAlphabet = "0123456789abcdefghijklmnopqrstuv";
+            string str = NumberHelper.FormatInt64To32Base(num, customAlphabet);
             Assert.Equal(expected, str);
         }
     }
--- LeaFLib/LeaFLib.Common.Core/NumberHelper.cs
+++ LeaFLib/LeaFLib.Common.Core/NumberHelper.cs
@@ -1,45 +1,100 @@
-﻿using System.Collections.Specialized;
-using System.Text;
-
-namespace LeaFLib.Common.Core
+﻿namespace LeaFLib.Common.Core
 {
     public static class NumberHelper
     {
-        private const string Bin32 = "0123456789abcdefghjkmnpqrstuwxyz";
+        private const string Default32BaseAlphabet = "0123456789abcdefghjkmnpqrstuwxyz";
 
-        public static string ConvertToBin32(int num, string? template = default)
+        public static string FormatInt32To32Base(int num, string? customAlphabet = default)
         {
-            if (string.IsNullOrEmpty(template))
+            if (num < 0)
             {
-                template = Bin32;
-            }
-            else if (template.Length < 32)
-            {
-                throw new ArgumentException("Length of template should be 32 at least");
+                throw new ArgumentOutOfRangeException(nameof(num), "num is less than 0");
             }
 
-            BitVector32 bits = new(num);
-            BitVector32.Section[] sections = new BitVector32.Section[7];
-            sections[6] = BitVector32.CreateSection(31);
-            sections[5] = BitVector32.CreateSection(31, sections[6]);
-            sections[4] = BitVector32.CreateSection(31, sections[5]);
-            sections[3] = BitVector32.CreateSection(31, sections[4]);
-            sections[2] = BitVector32.CreateSection(31, sections[3]);
-            sections[1] = BitVector32.CreateSection(31, sections[2]);
-            sections[0] = BitVector32.CreateSection(1, sections[1]);
-            StringBuilder sb = new();
-            bool ignoreZero = true;
-            foreach (var sec in sections)
+            string alphabet = Default32BaseAlphabet;
+            if (customAlphabet != null)
             {
-                int temp = bits[sec];
-                if (temp == 0 && ignoreZero)
+                if (customAlphabet.Length != 32)
                 {
-                    continue;
+                    throw new ArgumentException("The length of customAlphabet is not 32", nameof(customAlphabet));
                 }
-                sb.Append(template[temp]);
-                ignoreZero = false;
+                alphabet = customAlphabet;
             }
-            return sb.ToString();
+
+            if (num < 32)
+            {
+                return alphabet[num].ToString();
+            }
+
+            Span<char> chars = stackalloc char[7];
+            int temp = num & (1 << 30), offset;
+            chars[0] = alphabet[temp >> 30];
+            for (int i = 1; i < chars.Length; i++)
+            {
+                offset = 30 - i * 5;
+                temp = num & (31 << offset);
+                chars[i] = alphabet[temp >> offset];
+            }
+
+            if (chars[0] != alphabet[0])
+            {
+                return new string(chars);
+            }
+            for (int i = 1; i < chars.Length - 1; i++)
+            {
+                if (chars[i] != alphabet[0])
+                {
+                    return new string(chars[i..]);
+                }
+            }
+            return chars[^1].ToString();
+        }
+
+        public static string FormatInt64To32Base(long num, string? customAlphabet = default)
+        {
+            if (num < 0L)
+            {
+                throw new ArgumentOutOfRangeException(nameof(num), "num is less than 0");
+            }
+
+            string alphabet = Default32BaseAlphabet;
+            if (customAlphabet != null)
+            {
+                if (customAlphabet.Length != 32)
+                {
+                    throw new ArgumentException("The length of customAlphabet is not 32", nameof(customAlphabet));
+                }
+                alphabet = customAlphabet;
+            }
+
+            if (num < 32L)
+            {
+                return alphabet[(int)num].ToString();
+            }
+
+            Span<char> chars = stackalloc char[13];
+            long temp = num & (7L << 60);
+            chars[0] = alphabet[(int)(temp >> 60)];
+            int offset;
+            for (int i = 1; i < chars.Length; i++)
+            {
+                offset = 60 - i * 5;
+                temp = num & (31L << offset);
+                chars[i] = alphabet[(int)(temp >> offset)];
+            }
+
+            if (chars[0] != alphabet[0])
+            {
+                return new string(chars);
+            }
+            for (int i = 1; i < chars.Length - 1; i++)
+            {
+                if (chars[i] != alphabet[0])
+                {
+                    return new string(chars[i..]);
+                }
+            }
+            return chars[^1].ToString();
         }
     }
 }
