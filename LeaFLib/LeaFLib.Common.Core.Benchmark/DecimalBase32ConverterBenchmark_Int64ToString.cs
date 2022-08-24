51172e2a4967a9e19c49059de28229702c55d165
LeaFrock <petergeorge@126.com>
2022-08-24 10:26:28 +0800

    Add benchmark for `DecimalBase32Converter`

---
+++ LeaFLib/LeaFLib.Common.Core.Benchmark/DecimalBase32ConverterBenchmark_Int64ToString.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace LeaFLib.Common.Core.Benchmark
+{
+    [MemoryDiagnoser]
+    public class DecimalBase32ConverterBenchmark_Int64ToString
+    {
+        private readonly long _num;
+        private readonly DecimalBase32Converter _converter;
+
+        public DecimalBase32ConverterBenchmark_Int64ToString()
+        {
+            _num = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            _converter = DecimalBase32Converter.Default;
+        }
+
+        [Benchmark]
+        public string Int64ToString_32Base()
+        {
+            return _converter.ToString(_num);
+        }
+
+        [Benchmark(Baseline = true)]
+        public string Int64ToString_Hex()
+        {
+            return _num.ToString("x");
+        }
+    }
+}
--- LeaFLib/LeaFLib.Common.Core.Benchmark/Program.cs
+++ LeaFLib/LeaFLib.Common.Core.Benchmark/Program.cs
@@ -1,6 +1,6 @@
 ﻿using BenchmarkDotNet.Running;
 using LeaFLib.Common.Core.Benchmark;
 
-var _ = BenchmarkRunner.Run<NumberHelperBenchmark>();
+var _ = BenchmarkRunner.Run<DecimalBase32ConverterBenchmark_Int64ToString>();
 
 Console.ReadKey();
