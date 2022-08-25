c486466a893c74025d54361f70a0aaad10ee76b0
LeaFrock <petergeorge@126.com>
2022-08-25 14:28:38 +0800

    Add `StringHelper.RandomBase64String`

---
+++ src/LeaFLib.Common.Core/StringHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace LeaFLib.Common.Core
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Random a Base64 String
+        /// </summary>
+        /// <param name="length">The length of Base64 String</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException">The length is less than 1.</exception>
+        /// <exception cref="ArgumentException">The length is not a multiple of 4.</exception>
+        public static string RandomBase64String(int length)
+        {
+            if (length < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "The length is less than 1");
+            }
+            if (length % 4 != 0)
+            {
+                throw new ArgumentException("The length is not a multiple of 4", nameof(length));
+            }
+
+            int byteCount = length * 6 / 8;
+            Span<byte> bytes = byteCount <= 512
+                ? stackalloc byte[byteCount]
+                : new byte[byteCount];
+            Random.Shared.NextBytes(bytes);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
---
+++ tests/LeaFLib.Common.Core.UnitTest/StringHelperUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace LeaFLib.Common.Core.UnitTest
+{
+    public class StringHelperUnitTest
+    {
+        [Theory]
+        [InlineData(0, typeof(ArgumentOutOfRangeException))]
+        [InlineData(7, typeof(ArgumentException))]
+        [InlineData(4, default)]
+        [InlineData(1024, default)]
+        public void RandomBase64String(int length, Type exceptionType)
+        {
+            if (exceptionType is null)
+            {
+                string result = StringHelper.RandomBase64String(length);
+                Assert.Equal(length, result.Length);
+            }
+            else
+            {
+                Assert.Throws(exceptionType, () => StringHelper.RandomBase64String(length));
+            }
+        }
+    }
+}
