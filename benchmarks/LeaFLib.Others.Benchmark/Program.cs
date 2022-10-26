826284c409fc799b0ad902e8c7a62dd97d2f5a76
LeaFrock <petergeorge@126.com>
2022-10-26 17:47:30 +0800

    Add benchmarks collected from others

---
+++ benchmarks/LeaFLib.Others.Benchmark/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+using LeaFLib.Others.Benchmark;
+
+var _ = BenchmarkRunner.Run<StringBuilderBenchmark_Append>();
+
+// var _ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+Console.ReadKey();
---
+++ benchmarks/LeaFLib.Others.Benchmark/StringBuilderBenchmark_Append.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace LeaFLib.Others.Benchmark
+{
+    [MemoryDiagnoser]
+    public class StringBuilderBenchmark_Append
+    {
+        // It's from https://github.com/dotnet/aspnetcore/pull/44691/files#diff-85394d9486a1b4a71316a5f8c0859a1024ea7f56c84dd6eae83f3549526034a1
+
+        private readonly byte[] _b = new byte[30];
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            RandomNumberGenerator.Fill(_b);
+        }
+
+        [Benchmark]
+        public string AppendToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var b in _b)
+            {
+                sb.Append(b.ToString("x2", CultureInfo.InvariantCulture));
+            }
+            return sb.ToString();
+        }
+
+        [Benchmark]
+        public string AppendInterpolated()
+        {
+            var sb = new StringBuilder();
+            foreach (var b in _b)
+            {
+                sb.Append(CultureInfo.InvariantCulture, $"{b:x2}");
+            }
+            return sb.ToString();
+        }
+    }
+}
