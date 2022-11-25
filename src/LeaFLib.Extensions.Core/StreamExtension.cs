49bce8a8b2368a0365d2d5b12b3b251db961eaf1
LeaFrock <petergeorge@126.com>
2022-11-25 22:28:31 +0800

    Add `StreamExtension`

---
+++ src/LeaFLib.Extensions.Core/StreamExtension.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace LeaFLib.Extensions.Core
+{
+    /// <summary>
+    /// Extensions for <see cref="Stream"/>
+    /// </summary>
+    public static class StreamExtension
+    {
+        /// <summary>
+        /// Get current encoding of the stream by reading in sync
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static Encoding? GetEncodingSync(this Stream stream)
+        {
+            try
+            {
+                var pos = stream.Position;
+                using var reader = new StreamReader(stream, detectEncodingFromByteOrderMarks: true, bufferSize: 128, leaveOpen: true);
+                if (reader.Peek() > -1)
+                {
+                    _ = reader.Read();
+                    stream.Position = pos;
+                    return reader.CurrentEncoding;
+                }
+            }
+            catch (Exception)
+            {
+                // Just ignore it
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Get current encoding of the stream by reading in async
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static async Task<Encoding?> GetEncodingAsync(this Stream stream)
+        {
+            try
+            {
+                var pos = stream.Position;
+                using var reader = new StreamReader(stream, detectEncodingFromByteOrderMarks: true, bufferSize: 128, leaveOpen: true);
+                if (reader.Peek() > -1)
+                {
+                    _ = await reader.ReadAsync(new char[1]);
+                    stream.Position = pos;
+                    return reader.CurrentEncoding;
+                }
+            }
+            catch (Exception)
+            {
+                // Just ignore it
+            }
+            return default;
+        }
+    }
+}
